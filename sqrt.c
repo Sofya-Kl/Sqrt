03e6e9d8d3694e8e2b706c8e70857fdb8efc3686
Klepikova Sofya <sofja.klepikowa14@gmail.com>
2016-02-29 20:39:19 +0600

    Dded the condition for the discriminant

--- sqrt.c
+++ sqrt.c
@@ -6,5 +6,17 @@ int main()
 	double a,b,c,d,x1,x2;
 	printf("Enter a, b, c: ");
 	scanf("%f %f %f", &a, &b, &c);
+	d=b*b-4*a*c;
+	if (d=0){
+		x1=x2=-b/2*a;
+	}
+	if (d<0){
+		printf("no solution");
+	}
+	if (d>0){
+		x1=(-b+sqrt(d))/2*a;
+		x1=(-b-sqrt(d))/2*a;
+	}
+	
 	return 0;
 }
