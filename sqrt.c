7f38b4704f51be7074df30d41907a4e8b42024f5
Klepikova Sofya <sofja.klepikowa14@gmail.com>
2016-02-29 20:54:42 +0600

    Added print roots

--- sqrt.c
+++ sqrt.c
@@ -5,10 +5,11 @@ int main()
 {
 	double a,b,c,d,x1,x2;
 	printf("Enter a, b, c: ");
-	scanf("%f %f %f", &a, &b, &c);
+	scanf("%lf %lf %lf", &a, &b, &c);
 	d=b*b-4*a*c;
-	if (d=0){
-		x1=x2=-b/2*a;
+	if (d==0){
+		x1=-b/2*a;
+		printf("x1=x2=%.2lf.\n",x1,x1);
 	}
 	if (d<0){
 		printf("no solution");
@@ -16,6 +17,8 @@ int main()
 	if (d>0){
 		x1=(-b+sqrt(d))/2*a;
 		x1=(-b-sqrt(d))/2*a;
+		printf("x1=%.2lf.\n",x1);
+		printf("x1=%.2lf.\n",x1);
 	}
 	
 	return 0;
